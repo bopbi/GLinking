513050669b8e28dac5e5fd32ffcb65b7766a1f60
Bobby Prabowo <prabowo.bobby@gmail.com>
2024-04-03 21:40:44 +0900

    initial commit

---
+++ app/src/main/java/com/bobbyprabowo/android/googlelinking/ConsentActivity.kt
@@ -0,0 +1,62 @@
+package com.bobbyprabowo.android.googlelinking
+
+import android.app.Activity
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.GoogleAuthUtil.getToken
+import com.google.android.gms.auth.api.identity.SaveAccountLinkingTokenRequest
+
+class ConsentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val view = findViewById<View>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        intent?.let {
+            // Handle the intent
+            println(intent)
+        }
+    }
+
+    private fun onConsentAccepted() {
+        // Obtain a token (for simplicity, we’ll ignore the async nature
+        // of the following call)
+        val token = getToken(this, "oauth-learn", "oauth2:profile email")
+        val intent = Intent()
+            .putExtra(
+                SaveAccountLinkingTokenRequest.EXTRA_TOKEN,
+                token)
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
+
+    private fun onConsentRejectedOrCanceled() {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    companion object {
+        fun createPendingIntent(context: Context): PendingIntent {
+            return PendingIntent.getActivity(
+                context,
+                0,
+                Intent(context, ConsentActivity::class.java),
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/bobbyprabowo/android/googlelinking/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.bobbyprabowo.android.googlelinking
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SaveAccountLinkingTokenRequest
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val view = findViewById<View>(R.id.main)
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Create an ActivityResultLauncher which registers a callback for the
+        // Activity result contract
+        val activityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartIntentSenderForResult())
+        { result ->
+            if (result.resultCode == RESULT_OK) {
+                // Successfully finished the flow and saved the token
+                Log.i(TAG, "Success to save token result: ${result.resultCode}")
+            } else {
+                // Flow failed, for example the user may have canceled the flow
+                Log.e(TAG, "Failed to save token result: ${result.resultCode}")
+            }
+        }
+
+        findViewById<Button>(R.id.linkingButton).setOnClickListener {
+            openGoogleLink(activityResultLauncher)
+        }
+    }
+
+    private fun openGoogleLink(activityResultLauncher: ActivityResultLauncher<IntentSenderRequest>) {
+        val consentPendingIntent = ConsentActivity.createPendingIntent(this)
+
+        // Build token save request
+        val request = SaveAccountLinkingTokenRequest.builder()
+            .setTokenType(SaveAccountLinkingTokenRequest.TOKEN_TYPE_AUTH_CODE)
+            .setConsentPendingIntent(consentPendingIntent)
+            .setServiceId("xxxx")
+            //Set the scopes that the token is valid for on your platform
+            .setScopes(listOf("profile", "email"))
+            .build()
+
+        // Launch consent activity and retrieve token
+        Identity.getCredentialSavingClient(this)
+            .saveAccountLinkingToken(request)
+            .addOnSuccessListener { saveAccountLinkingTokenResult ->
+                if (saveAccountLinkingTokenResult.hasResolution()) {
+                    val pendingIntent = saveAccountLinkingTokenResult
+                        .pendingIntent
+                    pendingIntent?.let {
+                        // Start the intent sender to launch the consent flow
+                        val intentSenderRequest = IntentSenderRequest
+                            .Builder(pendingIntent).build()
+                        activityResultLauncher.launch(intentSenderRequest)
+                    } ?: run {
+                        // This should not happen, let’s log this
+                        Log.e(TAG, "Failed to save token")
+                    }
+                } else {
+                    // This should not happen, let’s log this
+                    Log.e(TAG, "Failed to save token")
+                }
+            }
+            .addOnFailureListener { e ->
+                Log.e(TAG, "Failed to save token", e)
+
+            }
+    }
+}
